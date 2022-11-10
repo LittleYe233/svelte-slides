d2a8d55302e454d64132cdcd108c55f741323f75
LittleYe233 <littleye233@gmail.com>
2022-11-10 16:43:26 +0800

    build(svelte): set up Svelte preprocessor

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
+import preprocess from 'svelte-preprocess';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import typescript from '@rollup/plugin-typescript';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte()]
+    plugins: [
+        svelte({
+            preprocess: [
+                preprocess({
+                    postcss: true
+                })
+            ]
+        }),
+        typescript()
+    ]
 });
